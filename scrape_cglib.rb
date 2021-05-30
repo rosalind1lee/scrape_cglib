1b8edf48cb1475d6c9fabebbcbecd30b4132bf9c
rosalind1lee <rlee90@chicagobooth.edu>
2021-05-30 00:25:11 +0000

    No such file or directory @ rb_sysopen - https://www.cglib.org/composers (Errno::ENOENT)

---
+++ scrape_cglib.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+#require 'uri'
+#require 'net/http'
+
+#doc = Nokogiri::HTML(open("https://www.cglib.org/composers"))
+#binding.pry
+
+url = 'https://www.cglib.org/composers'
+p url
+doc = Nokogiri::HTML(open(url).read)
